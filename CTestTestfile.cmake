f9c39efd679550d78db37776bdb0fef806506f67
Egor Bedov <bedovegor2203@gmail.com>
2019-10-11 00:28:41 +0300

    init commit

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/egorbedov/CLionProjects/TechnoPark-DeepCee
-# Build directory: /Users/egorbedov/CLionProjects/TechnoPark-DeepCee
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MainTest "/Users/egorbedov/CLionProjects/TechnoPark-DeepCee/IS1")
-set_tests_properties(MainTest PROPERTIES  PASS_REGULAR_EXPRESSION "OK" _BACKTRACE_TRIPLES "/Users/egorbedov/CLionProjects/TechnoPark-DeepCee/CMakeLists.txt;17;add_test;/Users/egorbedov/CLionProjects/TechnoPark-DeepCee/CMakeLists.txt;0;")
--- cmake-build-debug/CMakeFiles/IS1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for IS1.
-# This may be replaced when dependencies are built.
